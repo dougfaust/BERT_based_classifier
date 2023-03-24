a299d189e391d0f3e6325478f567bcce2b26d68c
dougfaust <faust.doug@gmail.com>
2023-03-24 09:56:51 -0700

    added load model method

--- classifier_BERT.py
+++ classifier_BERT.py
@@ -99,6 +99,18 @@ def evaluate(dataloader_val):
 
     return loss_val_avg, predictions, true_vals
 
+def load_model_from_file(model_path):
+    model = BertForSequenceClassification.from_pretrained("bert-base-uncased",
+                                                          num_labels=len(label_dict),
+                                                          output_attentions=False,
+                                                          output_hidden_states=False)
+
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    return model
 
 if __name__ == '__main__':
 
